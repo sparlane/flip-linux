c53d7d4efa8106b00421b4f0b07f3684389c3ee6
Scott Parlane <scott@parlanenz.com>
2026-02-28 22:04:27 +1300

    Add rpc port local/hereis into router

--- include/flip_router.hpp
+++ include/flip_router.hpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include "flip_proto.hpp"
 #include "netdrv.hpp"
+#include "rpc_port_manager.hpp"
 
 class flip_route_entry
 {
@@ -20,10 +21,14 @@ class flip_router
 {
 private:
     std::map<flip_address_t, std::shared_ptr<flip_route_entry>> routing_table;
+    std::shared_ptr<RpcPortManager> rpc_port_mgr;
     std::shared_ptr<flip_route_entry> find_route(flip_address_t dst);
+    void handle_rpc_locate(flip_address_t src_addr, flip_address_t dst_addr, const rpc_header* rpc_hdr, uint16_t actual_hopcount, const uint8_t* payload, size_t payload_len, flip_network_t incoming_network);
+    void handle_rpc_hereis(flip_address_t src_addr, const rpc_header* rpc_hdr);
 public:
     flip_router();
     ~flip_router();
     void route_packet(hwaddr_t src_mac, const uint8_t* packet, size_t len, flip_network_t incoming_network);
     void increment_age();
+    std::shared_ptr<RpcPortManager> get_rpc_port_manager() { return rpc_port_mgr; }
 };
