9ed018738d212da4230711b5030cd78c9dc4d3e6
Scott Parlane <scott@parlanenz.com>
2026-03-06 21:19:48 +1300

    Send RPC responses back to the client

--- include/flip_router.hpp
+++ include/flip_router.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <functional>
 #include <map>
 #include <memory>
 #include "flip_proto.hpp"
@@ -17,6 +18,10 @@ public:
     bool local;
 };
 
+// Called when a UNIDATA RPC reply is destined for a local address.
+// Parameters: dst flip address, payload after the rpc_header, payload length.
+using local_rpc_reply_cb = std::function<void(flip_address_t dst, const uint8_t* payload, size_t len)>;
+
 class flip_router
 {
 private:
@@ -24,9 +29,11 @@ private:
     std::shared_ptr<RpcPortManager> rpc_port_mgr;
     std::shared_ptr<flip_networks> networks;
     uint32_t locate_tid{0};
+    local_rpc_reply_cb on_local_rpc_reply;
     std::shared_ptr<flip_route_entry> find_route(flip_address_t dst);
     void handle_rpc_locate(flip_address_t src_addr, flip_address_t dst_addr, const rpc_header* rpc_hdr, uint16_t actual_hopcount, const uint8_t* payload, size_t payload_len, flip_network_t incoming_network);
     void handle_rpc_hereis(flip_address_t src_addr, const rpc_header* rpc_hdr);
+    void send_rpc_ack(flip_address_t src, flip_address_t dst, const rpc_header* original_rpc_hdr);
     void forward_broadcast(const uint8_t* packet, size_t len, flip_network_t incoming_network);
     void forward_unicast(const uint8_t* packet, size_t len, const hwaddr_t dst_mac, flip_network_t dst_network);
 public:
@@ -37,5 +44,6 @@ public:
     bool install_local_address(flip_address_t address);
     void remove_local_address(flip_address_t address);
     void send_rpc_locate(flip_address_t src_addr, const rpc_port_t& port);
+    void set_local_rpc_reply_cb(local_rpc_reply_cb cb) { on_local_rpc_reply = std::move(cb); }
     std::shared_ptr<RpcPortManager> get_rpc_port_manager() { return rpc_port_mgr; }
 };
